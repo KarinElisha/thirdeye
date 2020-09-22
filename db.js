5f0944977e275f8e36a7a770ab1249239b433129
arielbouskila <ariel.bouskila@gmail.com>
2020-09-22 23:26:52 +0300

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({
-    user: 'postgres',
-    host :'localhost',
-    database: 'thirdEye',
-    password: '1234',
-    port: 5432
-})
-module.exports = client;
